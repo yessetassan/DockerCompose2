cea8c3576b84fd7312adc874c02c303567a40142
yessetassan <139701904+yessetassan@users.noreply.github.com>
2024-02-06 11:53:46 +0600

    Update RestStudentTeacher.java

--- DockerCompose2/src/main/java/com/project/DockerCompose2/rest/RestStudentTeacher.java
+++ DockerCompose2/src/main/java/com/project/DockerCompose2/rest/RestStudentTeacher.java
@@ -18,6 +18,10 @@ public class RestStudentTeacher {
     private final SService sService;
     private final TService tService;
 
+    private Optional<Student> studentOpt;
+    private Optional<Teacher> teacherOpt;
+
+
     @Autowired
     public RestStudentTeacher(SService sService, TService tService) {
         this.sService = sService;
@@ -32,12 +36,11 @@ public class RestStudentTeacher {
     @PostMapping("/{s_id}/{t_id}")
     public ResponseEntity<String> save(@PathVariable(name = "s_id") Integer s_id,
                                        @PathVariable(name = "t_id") Integer t_id) {
-        Optional<Student> studentOpt = sService.findById(s_id);
-        Optional<Teacher> teacherOpt = tService.findById(t_id);
+        studentOpt = sService.findById(s_id);
+        teacherOpt = tService.findById(t_id);
 
-        if (!studentOpt.isPresent() || !teacherOpt.isPresent()) {
+        if (absent())
             return ResponseEntity.badRequest().body("Either student or teacher doesn't exist.");
-        }
 
         Student student = studentOpt.get();
         Teacher teacher = teacherOpt.get();
@@ -51,5 +54,29 @@ public class RestStudentTeacher {
         } else {
             return ResponseEntity.badRequest().body("Association already exists.");
         }
+    }
+
+    @PostMapping("/rm/{s_id}/{t_id}")
+    public ResponseEntity<String> rm(@PathVariable(name = "s_id") Integer s_id,
+                                       @PathVariable(name = "t_id") Integer t_id) {
+        studentOpt = sService.findById(s_id);
+        teacherOpt = tService.findById(t_id);
+
+        if (absent())
+            return ResponseEntity.badRequest().body("Either student or teacher doesn't exist.");
+
+        Student student = studentOpt.get();
+        Teacher teacher = teacherOpt.get();
+
+        Set<Teacher> teachers = student.getTeachers();
+        teachers.remove(teacher);
+        student.setTeachers(teachers);
+        sService.save(student);
+
+        return ResponseEntity.ok().body("Removed successfully !");
+    }
+
+    public boolean absent(){
+        return !studentOpt.isPresent() || !teacherOpt.isPresent();
     }
 }
